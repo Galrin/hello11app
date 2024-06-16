5bfa337bdab6cd661018dd2a8b2e6d894d027bdf
Galrin <elemental.gm@gmail.com>
2024-06-16 17:04:16 +0300

    update 3..

--- create_database.py
+++ create_database.py
@@ -4,5 +4,5 @@ from config import Config
 conn = pymysql.connect(host=Config.DATABASE_HOST,
                        user=Config.DATABASE_USER,
                        password=Config.DATABASE_PASSWORD)
-conn.cursor().execute('CREATE DATABASE ' + Config.DATABASE_NAME)
+conn.cursor().execute('CREATE DATABASE ' + Config.DATABASE_NAME + ' CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;')
 conn.close()
--- hello11app/main_books.py
+++ hello11app/main_books.py
@@ -1,10 +1,10 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for, jsonify
 )
 from pony.orm import desc
 from werkzeug.exceptions import abort
 
-from hello11app import Post
+from hello11app import Post, db
 from hello11app.auth import login_required, route_access
 
 bp = Blueprint("main_books", __name__)
@@ -23,3 +23,20 @@ books = {
 @route_access
 def index():
     return render_template("main/books.html")
+
+
+@bp.route('/main/api/book/')
+@bp.route('/main/api/book/<int:book_index>')
+def get_data(book_index = 1):
+    try:
+        rows = db.select(f"SELECT * FROM `{books[book_index]}`")
+        #for row in rows:
+        #    print(row[0], row[1], row[2])
+        print(jsonify(rows))
+
+    except Exception as e:
+        print("error", str(e))
+        return jsonify({"book_index": book_index, "book_name": books[book_index], "success": False, "error": str(e), "data": [1,2,3]})
+
+    return jsonify(
+        {"book_index": book_index, "success": True, "data": rows})
